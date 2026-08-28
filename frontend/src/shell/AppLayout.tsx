844dfd6d5a73f1d4051a3203d4048a2e777ce0a7
Grappe501 <grappe4arkansas@gmail.com>
2026-08-28 08:34:04 -0500

    CF-015 mount contextual Foundry navigation

--- frontend/src/shell/AppLayout.tsx
+++ frontend/src/shell/AppLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { CommandBar } from "./CommandBar";
 import { ContextPanel } from "./ContextPanel";
 import { DepartmentNav } from "./DepartmentNav";
+import { FoundryNav } from "./FoundryNav";
 import { SafetyBanner } from "./SafetyBanner";
 import { SystemStatusDock } from "./SystemStatusDock";
 
@@ -10,6 +11,7 @@ export function AppLayout() {
     <div className="os-shell">
       <CommandBar />
       <DepartmentNav />
+      <FoundryNav />
       <div className="os-shell__body">
         <main className="os-shell__main">
           <SafetyBanner />
